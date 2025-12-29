37a460cefb2d0bd098cf33e7e8732e50c4fcfbc8
CertainTingz <lyndonsimango@gmail.com>
2025-12-29 17:57:46 +0100

    Added a new Display Line+4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,6 +14,9 @@ public class Main {
             System.out.println("i = " + i);
             System.out.println("i = " + i+2);
             System.out.println("i = " + i+3);
+            System.out.println("i = " + i+4);
+
+
         }
     }
 }
